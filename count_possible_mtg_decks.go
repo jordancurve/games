4a28c9fbe945f23794aebba020df5c88200fbce9
khaaan <josh@joshjordan.name>
2017-07-28 16:11:08 -0400

    Count possible MtG decks in various formats

---
+++ count_possible_mtg_decks.go
@@ -0,0 +1,100 @@
+// Calculate the number off possible 60-card decks are there in various Magic the Gathering formats as of 2017-07-28.
+// Standard: 1.56e+115 (15557298208099665159797687556843375800891954968061261114749160258351379032066178441153374751887650643445771309006688)
+//  Modern: 6.81e+160 (68106617409207659782596899409913786481914176856602046141421661082408297654631243469713747354814673053856723338574045802958440474288109941644684438722934561721256)
+//  Legacy: 5.08e+171 (5080939832648988025064576494460412090389199717636167167171938112390361763302568603423099038486932271443146821054192886023291494585938272143349131282700607841814088785453620)
+// Vintage: 6.04e+171 (6043408612825080680432868145170719616809553529353154350849048487329020297630950194253272730067699771949011891037033955336864555350748114282957356813363059378572337387986680)
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"strings"
+)
+
+// Cache key, used to speed up the computation.
+type key struct {
+	numToBuy, numProducts int
+}
+
+type Legality struct {
+	Format, Legality string
+}
+
+type Card struct {
+	Name       string
+	Type       string
+	Legalities []Legality
+}
+
+func main() {
+	limits := FormatLimits("AllCards-x.json") // from https://mtgjson.com/json/AllCards-x.json.zip
+	formats := []string{"Standard", "Modern", "Legacy", "Vintage"}
+	for _, f := range formats {
+		c := LimitedMultiChoose(60, limits[f])
+		fmt.Printf("%8s: %.3g (%v)\n", f, new(big.Float).SetInt(c), c)
+	}
+}
+
+func FormatLimits(mtgJsonFile string) map[string][]int {
+	mtgJson, err := ioutil.ReadFile(mtgJsonFile)
+	if err != nil {
+		panic(err)
+	}
+	var cards map[string]Card
+	if err := json.Unmarshal(mtgJson, &cards); err != nil {
+		panic(err)
+	}
+	limits := map[string][]int{}
+	for _, c := range cards {
+		for _, leg := range c.Legalities {
+			f := leg.Format
+			if _, ok := limits[f]; !ok {
+				limits[f] = []int{}
+			}
+			lim := 0
+			if leg.Legality == "Legal" {
+				if strings.HasPrefix(c.Type, "Basic Land") {
+					lim = 1000
+				} else {
+					lim = 4
+				}
+			} else if leg.Legality == "Restricted" {
+				lim = 1
+			}
+			if lim > 0 {
+				limits[f] = append(limits[f], lim)
+			}
+		}
+	}
+	return limits
+}
+
+// LimitedMultiChoose(B, L) returns the number of ways to buy B items from a
+// store with len(L) products, where item I has only L[I] in stock (0 < I < N).
+// For example, LimitedMultiChoose(5, []int{3,4,6})=17, which is the number of
+// ways to choose 5 items to buy from a selection of 3 products, where product
+// 0 has 3 in stock, product 1 has 4 in stock, and product 2 has 6 in stock.
+func LimitedMultiChoose(numToBuy int, numInStock []int) *big.Int {
+	return _limitedMultiChoose(numToBuy, numInStock, map[key]*big.Int{})
+}
+
+func _limitedMultiChoose(numToBuy int, numInStock []int, cache map[key]*big.Int) *big.Int {
+	if numToBuy == 0 {
+		return big.NewInt(1)
+	}
+	if numToBuy < 0 || len(numInStock) == 0 {
+		return big.NewInt(0)
+	}
+	key := key{numToBuy, len(numInStock)}
+	if val, ok := cache[key]; ok {
+		return val
+	}
+	sum := big.NewInt(0)
+	for i := 0; i <= numInStock[0]; i++ {
+		sum.Add(sum, _limitedMultiChoose(numToBuy-i, numInStock[1:], cache))
+	}
+	cache[key] = sum
+	return sum
+}
